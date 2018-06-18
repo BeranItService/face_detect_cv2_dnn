e8b875c2f65a69846a2a99338dcf5b7327cfc077
kawin7538 <kawin7538@hotmail.com>
2018-06-18 20:04:25 +0700

    Add files via upload

---
+++ face_detect_cv2_dnn_realtime.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 18 17:49:05 2018
+
+@author: Kawin-PC
+"""
+
+import cv2
+import numpy as np
+
+model_location="res10_300x300_ssd_iter_140000.caffemodel"
+prototxt_location="deploy.prototxt.txt"
+
+net=cv2.dnn.readNetFromCaffe(prototxt_location,model_location)
+
+cap=cv2.VideoCapture(0)
+while cap.isOpened():
+    
+    ret,frame=cap.read()
+    
+    if ret==False:
+        break
+    
+    (h,w) = frame.shape[:2]
+    blob=cv2.dnn.blobFromImage(cv2.resize(frame,(300,300)),1.0,(300,300),(104.0,177.0,123.0))
+    net.setInput(blob)
+    detections=net.forward()
+    
+    for i in range(0,detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence<0.5:
+            continue
+        box=detections[0,0,i,3:7]*np.array([w,h,w,h])
+        (startX,startY,endX,endY)=box.astype("int")
+        text="{:.2f}%".format(confidence*100)
+        y=startY-10 if startY-10>10 else startY+10
+        cv2.rectangle(frame,(startX,startY),(endX,endY),(0,255,0),2)
+        cv2.putText(frame,text,(startX,y),cv2.FONT_HERSHEY_SIMPLEX,0.45,(0,255,0),2)
+        
+    
+    cv2.imshow('frame',frame)
+    
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+    
---
+++ face_detect_cv2_dnn_video.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 18 18:28:01 2018
+
+@author: Kawin-PC
+"""
+
+import cv2
+import numpy as np
+import time
+
+video_location="videoplayback.mp4"
+prototxt_location="deploy.prototxt.txt"
+model_location="res10_300x300_ssd_iter_140000.caffemodel"
+
+cap=cv2.VideoCapture(video_location)
+net=cv2.dnn.readNetFromCaffe(prototxt_location,model_location)
+
+start=time.time()
+
+while True:
+    ret,frame=cap.read()
+    
+    if ret == False:
+        break
+    
+    #frame=cap.read()
+    
+    (h,w)=frame.shape[:2]
+    blob=cv2.dnn.blobFromImage(cv2.resize(frame,(300,300)),1.0,(300,300),(104,0,177.0,123.0))
+    net.setInput(blob)
+    detections=net.forward()
+    
+    for i in range(0,detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence<=0.5:
+            continue
+        box=detections[0,0,i,3:7]*np.array([w,h,w,h])
+        (startX,startY,endX,endY)=box.astype("int")
+        text="{:.2f}%".format(confidence*100)
+        y=startY-10 if startY-10>10 else startY+10
+        cv2.rectangle(frame,(startX,startY),(endX,endY),(0,255,0),2)
+        cv2.putText(frame,text,(startX,y),cv2.FONT_HERSHEY_SIMPLEX,0.45,(0,255,0),2)
+        
+    
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+    blob=""
+    detections=""
+    h=""
+    w=""
+    frame=""
+
+cap.release()
+cv2.destroyAllWindows()
+#cap.stop()
+end=time.time()
+print("{:.2f}".format(end-start))
